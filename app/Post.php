163d056674cf687ad88309995b4b2c2bb4393ca8
mdeias <mdeias@icloud.com>
2022-02-07 23:38:57 +0100

    migration e seeder tabella ponte

--- app/Post.php
+++ app/Post.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
@@ -12,6 +11,10 @@ class Post extends Model
     public function category(){
 
         return $this->belongsTo('App\Category');
+    }
+
+    public function tags(){
+        return $this->belongsToMany('App\Tag');
     }
 
     protected $fillable = ['title', 'content', 'slug', 'category_id'];
